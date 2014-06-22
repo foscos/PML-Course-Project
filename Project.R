bfc5d12eb5db9981072ff6a17ad7fc2263883430
foscos <oscar.pascual.g@gmail.com>
2014-06-22 19:57:37 +0200

    Update Project.R

--- Project.R
+++ Project.R
@@ -1,5 +1,6 @@
 library(caret)
-#Columns in the training dataset with missing values are removed
+#Loading the caret library and reading the training dataset
+#Columns with missing values are removed
 data <- read.csv("pml-training.csv", header = TRUE, na.strings = c("NA", ""))
 csums <- colSums(is.na(data))
 cs <- (csums == 0)
@@ -10,10 +11,14 @@ traindata <- data[trainIndex,]
 removeIndex <- grep("timestamp|X|user_name|new_window",names(traindata))
 traindata <- traindata[,-removeIndex]
 
+#Training the model using a random forest approach on the train data partition
+#Using a cross validation method when applying the random forest routine 
 modFit <- train(traindata$classe ~.,  method = "rf", data = traindata, trControl = trainControl(method = "cv", number = 4), importance = TRUE)
 
+#Reading the testing dataset and removing columns with NAs
 data2 <- read.csv("pml-testing.csv", header = TRUE, na.strings = c("NA", ""))
 data2 <- data2[, (colSums(is.na(data)) == 0)]
 
+#Finally, predicting values on the testing dataset after removing the column 54 (that one makes problem)
 pred <- predict(modFit, data2[,-54])
 pred
